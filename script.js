33df1975e483df46650ac0f024f3199b015fbb15
Ruma Das <rumaadl16@gmail.com>
2020-03-17 12:29:14 +1030

    adding show and hide in script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   
+    $('#btnstart').click(function(){
+        $('#choice').show();
+        $('#quizescore').show();
+        $('#btnstart').hide();
+        })
+
+
+
+});
