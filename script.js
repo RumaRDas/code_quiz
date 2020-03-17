bb0ff5c2ddc9ef3fef57725ad799c8bd25f8b1e8
Ruma Das <rumaadl16@gmail.com>
2020-03-17 12:43:51 +1030

    adding question ,choices and answer in script.js file

--- script.js
+++ script.js
@@ -4,8 +4,28 @@ $(document).ready(function(){
         $('#choice').show();
         $('#quizescore').show();
         $('#btnstart').hide();
-        })
-
+        });
+        var questions = [{
+            question: "1.jQuery is a ?",
+            choices: ["JavaScript Library", "JavaScript Language", "JavaScript Method", "PHP Method"],
+            correctAnswer: 0
+        }, {
+            question: "2. Which jQuery method is used to hide selected elements?",
+            choices: [" hidden()", "hide()", "visible(false)", "display(none)"],
+            correctAnswer: 1
+        }, {
+            question: "3. Which jQuery method is used to set one or more style properties for selected elements",
+            choices: ["css()", " html()", "style()", "method()"],
+            correctAnswer: 0
+        }, {
+            question: "4. What scripting language is jQuery written in?",
+            choices: ["VBScript", "JavaScript", "C#", "C++"],
+            correctAnswer: 1
+        }, {
+            question: "5. Which jQuery method should be used to deal with name conflicts?",
+            choices: ["noNameConflict()", "noConflict()", "nameConflict()", "conflict()"],
+            correctAnswer: 3
+        }]
 
 
 });
